e1c6a31622bf9f61ce1c54fabdb59592592fd3e4
Benjamin Bergstrom <benko.dubovecky@gmail.com>
2022-06-29 16:11:33 +0200

    more operations

--- main.c
+++ main.c
@@ -11,20 +11,32 @@ int main(int argc, char const *argv[]) {
     matrix out2, *m2;
     m2 = &out2;
 
-    double arr1[] = {1, 2, 4, 2, 6, 0};
-    double arr2[] = {4, 1, 4, 3, 0, -1, 3, 1, 2, 7, 5, 2};
+    double arr1[] = {-1, 0, 2, 3};
+    double arr2[] = {1, 2, 3, 0};
     
-    matrix_init(2, 3, m1);
-    matrix_init(3, 4, m2);
-    matrix_mass_assign(arr1, 6, m1);
-    matrix_mass_assign(arr2, 12, m2);
+    matrix_init(2, 2, m1);
+    matrix_init(2, 2, m2);
+    matrix_mass_assign(arr1, 4, m1);
+    matrix_mass_assign(arr2, 4, m2);
 
-    matrix product = matrix_product(m1, m2);
-    matrix_print(&product);
+    matrix sum = matrix_sum(m1, m2);
+    matrix_print(&sum);
+    matrix_free(&sum);
+
+    matrix diff = matrix_diff(m1, m2);
+    matrix_print(&diff);
+    matrix_free(&diff);
+
+
+    matrix scalar_product = matrix_scalar_product(-3, m1);
+    matrix_print(&scalar_product);
+
+    matrix identity = identity_matrix(5);
+    matrix_print(&identity);
 
     matrix_free(m1);
     matrix_free(m2);
-    matrix_free(&product);
+    matrix_free(&scalar_product);
 
     return EXIT_SUCCESS;
 }
--- operations.h
+++ operations.h
@@ -1,10 +1,57 @@
 #include "matrix.h"
 #include "stdlib.h"
 
+matrix matrix_sum(matrix *m1, matrix *m2);
+matrix matrix_diff(matrix *m1, matrix *m2);
+matrix matrix_scalar_product(int scalar, matrix *m);
+matrix matrix_product(matrix *m1, matrix *m2);
+matrix identity_matrix(int n);
+
+matrix matrix_sum(matrix *m1, matrix *m2) {
+    if (m1->rows != m2->rows || m1->cols != m2->cols) {
+        fputs("These matrices cannot be added", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    matrix out, *sum;
+    sum = &out;
+
+    matrix_init(m1->rows, m1->cols, sum);
+
+    for (int i = 0; i < m1->rows; i++) {
+        for (int j = 0; j < m1->cols; j++) {
+            matrix_assign(i, j, matrix_get(i, j, m1) + matrix_get(i, j, m2), sum);
+        }
+    }
+
+    return *sum;
+}
+
+matrix matrix_diff(matrix *m1, matrix *m2) {
+    matrix diff = matrix_scalar_product(-1, m2);
+    return matrix_sum(m1, &diff);
+}
+
+matrix matrix_scalar_product(int scalar, matrix *m) {
+    matrix out, *product;
+    product = &out;
+
+    matrix_init(m->rows, m->cols, product);
+    
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            double value = matrix_get(i, j, m);
+            matrix_assign(i, j, scalar * value, product);
+        }
+    }
+
+    return *product;
+}
+
 matrix matrix_product(matrix *m1, matrix *m2) {
     if (m1->cols != m2->rows) {
         fputs("These matrices cannot be multiplied\n", stderr);
-        exit(EXIT_SUCCESS);
+        exit(EXIT_FAILURE);
     }
 
     matrix out, *product;
@@ -33,4 +80,27 @@ matrix matrix_product(matrix *m1, matrix *m2) {
     }
 
     return *product;
+}
+
+matrix identity_matrix(int n) {
+    if (n < 1) {
+        fprintf(stderr, "Invalid matrix size");
+        exit(EXIT_FAILURE);
+    }
+
+    matrix *identity;
+    matrix_init(n, n, identity);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) {
+                matrix_assign(i, j, 1, identity);
+                continue;
+            }
+
+            matrix_assign(i, j, 0, identity);
+        }
+    }
+
+    return *identity;
 }
